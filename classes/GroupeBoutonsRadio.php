3094a1e20a3134cf8e78a4babea044a1b78d58d8
Pier-Luc Brault <pier-luc@brault.me>
2025-04-15 10:13:25 -0400

    Classe ChampOptions

--- classes/GroupeBoutonsRadio.php
+++ classes/GroupeBoutonsRadio.php
@@ -1,32 +1,10 @@
 <?php
 
-require_once('Champ.php');
+require_once('ChampOptions.php');
 
-class GroupeBoutonsRadio extends Champ {
-    private $options;
-
+class GroupeBoutonsRadio extends ChampOptions {
     public function __construct($nom, $libelle, $estObligatoire) {
         parent::__construct($nom, $libelle, $estObligatoire);
-
-        $this->options = [];
-    }
-
-    public function ajouterOption($option) {
-        $this->options[] = $option;
-    }
-
-    public function valider() {
-        if ($this->estRecu()) {
-            $valeur = $this->getValeur();
-            if ($this->estObligatoire && empty($valeur)) {
-                $this->erreur = "Le champ {$this->libelle} est obligatoire.";
-            }
-            if (!in_array($valeur, $this->options)) {
-                $this->erreur = "Le champ {$this->libelle} doit être une option valide.";
-            }
-        } else if ($this->estObligatoire) {
-            $this->erreur = "Le champ {$this->libelle} est obligatoire.";
-        }
     }
 
     public function html() {
--- classes/ListeDeroulante.php
+++ classes/ListeDeroulante.php
@@ -1,31 +1,10 @@
 <?php
 
-require_once('Champ.php');
+require_once('ChampOptions.php');
 
-class ListeDeroulante extends Champ {
-    private $options;
-
+class ListeDeroulante extends ChampOptions {
     public function __construct($nom, $libelle, $estObligatoire) {
         parent::__construct($nom, $libelle, $estObligatoire);
-        $this->options = [];
-    }
-
-    public function ajouterOption($texte) {
-        $this->options[] = $texte;
-    }
-
-    public function valider() {
-        if ($this->estRecu()) {
-            $valeur = $this->getValeur();
-            if ($this->estObligatoire && empty($valeur)) {
-                $this->erreur = "Le champ {$this->libelle} est obligatoire.";
-            }
-            if (!in_array($valeur, $this->options)) {
-                $this->erreur = "Le champ {$this->libelle} doit être une option valide.";
-            }
-        } else if ($this->estObligatoire) {
-            $this->erreur = "Le champ {$this->libelle} est obligatoire.";
-        }
     }
 
     public function html() {
